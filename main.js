f07218ed0f40c05ce3a351022e48af8cb8fbd48b
vir$i <egrvrb@gmail.com>
2026-04-23 09:55:44 +0300

    fix

--- main.js
+++
@@ -1,7 +0,0 @@
-import {MainPage} from "./pages/main/index.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    const mainPage = new MainPage(root);
-    mainPage.render();
-});
